e59f0af9c7f1187a7d57d4a368ddc414614cdd6e
YOGENDRA311 <yoyosirr@gmail.com>
2021-08-16 14:17:53 +0530

    final

--- js/post.js
+++ js/post.js
@@ -9,13 +9,34 @@ const API_BASE_URL = "http://localhost:3000/";
 
 window.onload = () => {
     getPost();
+    getPostIdParam();
+}
+const getPostIdParam =()=>{
+    const queryString = window.location.search;
+    const urlParam = new URLSearchParams(queryString);
+    return urlParam.get("id");
 }
 
 const getPost = () => {
     // CODE GOES HERE
+    const postId = getPostIdParam();
+    const  url = `${API_URL}${postId}`
+    fetch(url,{
+        method:'GET'
+    }).then((response)=>{
+        return response.json()
+    }).then((data)=>{
+        buildPost(data);
+    })
 }
 
 const buildPost = (data) => {
+    const postDate = new Date(parseInt(data.added_date)).toString();
+    const postImage = `${API_BASE_URL}${data.post_image}`;
+    document.querySelector("header").style.backgroundImage = `url(${postImage})`
+    document.getElementById("indivisual-post-title").innerText = data.title;
+    document.getElementById("indivisual-post-date").innerText = `Published on ${postDate}`;
+    document.getElementById("indivisual-post-content").innerText = data.content;
     // HINT: Convert the date number to a Date string 
 }
 
